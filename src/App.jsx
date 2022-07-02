c06b7e08b0af6418eb0a20c766e6fa7563146879
Tony Bengué <tonybengue@hotmail.fr>
2022-07-02 02:00:00 +0200

    Connect App url and add links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hi React</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
