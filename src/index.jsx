d1bbf2ac181cb631fc79a7ae73f8df9bb79369ce
Tony Bengué <tonybengue@hotmail.fr>
2022-07-03 04:00:00 +0200

    Add routes and pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
+import Expenses from "../pages/Expenses"
+import Invoices from "../pages/Invoices"
 
 const rootElement = document.getElementById("root")
 const root = createRoot(rootElement)
 
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
   </BrowserRouter>
 )
